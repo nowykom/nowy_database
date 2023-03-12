70d08eba87a43a62a5838088b62974dfcc31179b
Tobias Schulz <tobias.schulz@linux.com>
2023-03-12 16:49:51 +0100

    Update Build.cs

--- nukebuild/Build.cs
+++ nukebuild/Build.cs
@@ -189,7 +189,7 @@ class Build : NukeBuild
                 {
                     DotNetTasks.DotNetNuGetPush(
                         _ => _
-                            .SetSource(nameof(NuGetOrgSource))
+                            .SetSource(NuGetOrgSource)
                             .SetApiKey(NuGetOrgApiKey)
                             .EnableSkipDuplicate()
                             .SetTargetPath(file)

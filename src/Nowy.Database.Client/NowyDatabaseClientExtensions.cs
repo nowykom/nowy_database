8c087c2cbe745ebf54f5019a2afe7db935169e33
Tobias Schulz <tobias.schulz@linux.com>
2023-04-03 14:06:13 +0200

    Update NowyDatabaseClientExtensions.cs

--- src/Nowy.Database.Client/NowyDatabaseClientExtensions.cs
+++ src/Nowy.Database.Client/NowyDatabaseClientExtensions.cs
@@ -10,8 +10,9 @@ public static class NowyDatabaseClientExtensions
 {
     public static void AddNowyDatabaseClient(this IServiceCollection services, string endpoint, Func<IServiceProvider, INowyDatabaseAuthService> func_database_auth_service)
     {
-        services.AddSingleton<INowyDatabaseAuthService>(func_database_auth_service);
         services.AddSingleton<IModelService, ModelService>(sp => new ModelService());
+
+        services.AddTransient<INowyDatabaseAuthService>(func_database_auth_service);
         services.AddTransient<INowyDatabase>(sp => new RestNowyDatabase(
             sp.GetRequiredService<IHttpClientFactory>(),
             sp.GetRequiredService<INowyDatabaseAuthService>(),

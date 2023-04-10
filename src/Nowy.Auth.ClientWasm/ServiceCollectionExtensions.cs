2e2c2463e85cd0b16125a5cf0d300dd5c5c28b21
Tobias Schulz <tobias.schulz@linux.com>
2023-04-11 00:11:43 +0200

    Update ServiceCollectionExtensions.cs

--- src/Nowy.Auth.ClientWasm/ServiceCollectionExtensions.cs
+++ src/Nowy.Auth.ClientWasm/ServiceCollectionExtensions.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Nowy.Auth.Client;
 
 namespace Nowy.Auth.ClientWasm;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddNowyAuthClientWasm(this IServiceCollection services)
+    public static void AddNowyAuthClientWasm(this IServiceCollection services, string endpoint)
     {
-
+        services.AddNowyAuthClient(endpoint: endpoint);
     }
 }

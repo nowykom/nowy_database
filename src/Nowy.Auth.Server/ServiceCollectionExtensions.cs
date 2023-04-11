86bde0bba59553af8bc63e2c4020e1e26718f1da
Tobias Schulz <tobias.schulz@linux.com>
2023-04-12 01:33:17 +0200

    Update ServiceCollectionExtensions.cs

--- src/Nowy.Auth.Server/ServiceCollectionExtensions.cs
+++ src/Nowy.Auth.Server/ServiceCollectionExtensions.cs
@@ -1,9 +1,11 @@
-﻿using System.Text;
+﻿using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Nowy.Auth.Common.Services;
+using Nowy.Auth.Contract.Models;
 using Nowy.Auth.Contract.Services;
 using Nowy.Auth.Server.Services;
 using Nowy.Database.Contract.Services;
@@ -57,10 +59,65 @@ public static class ServiceCollectionExtensions
         services.AddTransient<IUserPermissionService, DefaultUserPermissionService>();
     }
 
+    public static void AddNowyAuthServer2(this IServiceCollection services, string? database_name = null)
+    {
+        services.AddSingleton<INowyAuthService, DefaultNowyAuthService>();
+        services.AddSingleton<INowyDatabaseAuthService, DefaultNowyDatabaseAuthService>();
+
+        services.AddAuthentication(opt =>
+        {
+            opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.IncludeErrorDetails = true; // <- great for debugging
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                RequireSignedTokens = true,
+                RequireExpirationTime = true, // <- JWTs are required to have "exp" property set
+                ValidateLifetime = true, // <- the "exp" will be validated
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "nowy",
+                ValidAudience = "nowy",
+                IssuerSigningKey = KeyHelper.GetSymmetricKey(),
+            };
+        });
+
+        services.AddAuthorization();
+
+        services.AddTransient<IUserRepository, X1>();
+        services.AddTransient<IUserPermissionService, X2>();
+    }
+
     public static void UseNowyAuthServer(this WebApplication app)
     {
         app.UseAuthentication();
         app.UseMiddleware<PermissionsMiddleware>(); // our custom permission middleware
         app.UseAuthorization();
+    }
+
+    public class X1 : IUserRepository
+    {
+        public async ValueTask<IUserModel?> FindUserAsync(string name)
+        {
+            return null;
+        }
+
+        public async ValueTask<IReadOnlyList<IUserModel>> FindUsersAsync(string name)
+        {
+            return new IUserModel[] { };
+        }
+    }
+
+    public class X2 : IUserPermissionService
+    {
+        public async ValueTask<ClaimsIdentity?> GetUserPermissionsIdentity(string sub, CancellationToken cancellationToken)
+        {
+            return new ClaimsIdentity(new Claim[] { });
+        }
     }
 }

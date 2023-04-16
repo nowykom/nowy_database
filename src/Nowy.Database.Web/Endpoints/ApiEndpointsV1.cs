802f17d45d4f8c6d185de2aa6645058d59a6e21f
Tobias Schulz <tobias.schulz@linux.com>
2023-04-16 16:34:29 +0200

    Update ApiEndpointsV1.cs

--- src/Nowy.Database.Web/Endpoints/ApiEndpointsV1.cs
+++ src/Nowy.Database.Web/Endpoints/ApiEndpointsV1.cs
@@ -19,12 +19,14 @@ public class ApiEndpointsV1
     {
         app.MapGet("{database_name}/{entity_name}", GetAllAsync);
         app.MapGet("{database_name}/{entity_name}/{id}", GetByIdAsync);
-        app.MapPost("{database_name}/{entity_name}/{id}", UpdateAsync);
-        app.MapPut("{database_name}/{entity_name}/{id}", UpdateAsync);
+        app.MapPost("{database_name}/{entity_name}/{id}", UpsertAsync);
+        app.MapPut("{database_name}/{entity_name}/{id}", UpsertAsync);
         app.MapDelete("{database_name}/{entity_name}/{id}", DeleteAsync);
     }
 
-    public async Task<Results<FileContentHttpResult, NotFound>> GetAllAsync(MongoRepository repo, string database_name, string entity_name)
+    public async Task<Results<FileContentHttpResult, NotFound>> GetAllAsync(
+        MongoRepository repo, string database_name, string entity_name
+    )
     {
         _logger.LogInformation($"{nameof(GetAllAsync)}: {nameof(database_name)} = {database_name}, {nameof(entity_name)} = {entity_name}");
 
@@ -34,7 +36,9 @@ public class ApiEndpointsV1
         return TypedResults.Bytes(stream.ToArray(), "application/json");
     }
 
-    public async Task<Results<FileContentHttpResult, NoContent, NotFound>> GetByIdAsync(MongoRepository repo, string database_name, string entity_name, string id)
+    public async Task<Results<FileContentHttpResult, NoContent, NotFound>> GetByIdAsync(
+        MongoRepository repo, string database_name, string entity_name, string id
+    )
     {
         _logger.LogInformation($"{nameof(GetByIdAsync)}: {nameof(database_name)} = {database_name}, {nameof(entity_name)} = {entity_name}, {nameof(id)} = {id}");
 
@@ -51,8 +55,10 @@ public class ApiEndpointsV1
         }
     }
 
-    public async Task<Results<FileContentHttpResult, NotFound>> UpdateAsync(MongoRepository repo, string database_name, string entity_name, string id,
-        HttpRequest request)
+    public async Task<Results<FileContentHttpResult, NotFound>> UpsertAsync(
+        MongoRepository repo, string database_name, string entity_name, string id,
+        HttpRequest request
+    )
     {
         string input_json;
         await using (MemoryStream input_json_stream = new MemoryStream(2048))
@@ -62,18 +68,20 @@ public class ApiEndpointsV1
         }
 
         _logger.LogInformation(
-            $"{nameof(UpdateAsync)}: {nameof(database_name)} = {database_name}, {nameof(entity_name)} = {entity_name}, {nameof(id)} = {id}, {nameof(input_json)} = {input_json}");
+            $"{nameof(this.UpsertAsync)}: {nameof(database_name)} = {database_name}, {nameof(entity_name)} = {entity_name}, {nameof(id)} = {id}, {nameof(input_json)} = {input_json}");
 
         BsonDocument input_document = BsonDocument.Parse(input_json);
         BsonDocument? document = await repo.UpsertAsync(database_name: database_name, entity_name: entity_name, id: id, input: input_document);
         await using MemoryStream stream = await document.MongoToJsonStream();
 
-        _logger.LogInformation($"{nameof(UpdateAsync)}: => {Encoding.UTF8.GetString(stream.ToArray())}");
+        _logger.LogInformation($"{nameof(this.UpsertAsync)}: => {Encoding.UTF8.GetString(stream.ToArray())}");
 
         return TypedResults.Bytes(stream.ToArray(), "application/json");
     }
 
-    public async Task<Results<FileContentHttpResult, NoContent, NotFound>> DeleteAsync(MongoRepository repo, string database_name, string entity_name, string id)
+    public async Task<Results<FileContentHttpResult, NoContent, NotFound>> DeleteAsync(
+        MongoRepository repo, string database_name, string entity_name, string id
+    )
     {
         _logger.LogInformation($"{nameof(DeleteAsync)}: {nameof(database_name)} = {database_name}, {nameof(entity_name)} = {entity_name}, {nameof(id)} = {id}");
 

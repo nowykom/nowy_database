7661736ffaa5fd06071800a59adcaca49331bc74
Tobias Schulz <tobias.schulz@linux.com>
2023-06-26 01:54:28 +0200

    Update build-docker.sh

--- src/build-docker.sh
+++ src/build-docker.sh
@@ -2,9 +2,10 @@
 
 set -euxo pipefail
 
-docker image build -f Nowy.Database.Web/Dockerfile .. -t tobiasschulzdev/nowy_database:latest
+docker image build -f Nowy.Database.Web/Dockerfile .. -t tobiasschulzdev/nowy_database:latest -t tobiasschulzdev/lr_database:latest
 
 docker push tobiasschulzdev/nowy_database:latest
+docker push tobiasschulzdev/lr_database:latest
 
-ssh -o StrictHostKeyChecking=no root@mercury.leuchtraketen.cloud 'kubectl rollout restart deploy -n nowy_database'
+ssh -o StrictHostKeyChecking=no root@mercury.leuchtraketen.cloud 'kubectl rollout restart deploy -n lr_database'
 

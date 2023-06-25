dcbefc4689b3760dd0f3bf874559c7a6e42842e1
Tobias Schulz <tobias.schulz@linux.com>
2023-06-26 01:51:44 +0200

    Update build-docker.sh

--- src/build-docker.sh
+++ src/build-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 docker image build -f Nowy.Database.Web/Dockerfile .. -t tobiasschulzdev/nowy_database:latest
 
 docker push tobiasschulzdev/nowy_database:latest

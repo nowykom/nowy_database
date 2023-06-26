179b4cdca35119871d889a46a44d8387d508899b
Tobias Schulz <tobias.schulz@linux.com>
2023-06-26 02:37:22 +0200

    Update build-docker.sh

--- src/build-docker.sh
+++ src/build-docker.sh
@@ -5,10 +5,10 @@ set -euxo pipefail
 docker image build -f Nowy.Database.Web/Dockerfile ..   -t tobiasschulzdev/nowy_database:latest   -t tobiasschulzdev/lr_database:latest
 docker image build -f nowy_messagehub/Dockerfile ..     -t tobiasschulzdev/nowy_messagehub:latest -t tobiasschulzdev/lr_messagehub:latest
 
-docker push tobiasschulzdev/nowy_database:latest
-docker push tobiasschulzdev/lr_database:latest
-docker push tobiasschulzdev/nowy_messagehub:latest
-docker push tobiasschulzdev/lr_messagehub:latest
+retry --until=success --times=5 --delay=5 docker push tobiasschulzdev/nowy_database:latest
+retry --until=success --times=5 --delay=5 docker push tobiasschulzdev/lr_database:latest
+retry --until=success --times=5 --delay=5 docker push tobiasschulzdev/nowy_messagehub:latest
+retry --until=success --times=5 --delay=5 docker push tobiasschulzdev/lr_messagehub:latest
 
 ssh -o StrictHostKeyChecking=no root@mercury.leuchtraketen.cloud 'kubectl rollout restart deploy -n lr_database'
 ssh -o StrictHostKeyChecking=no root@mercury.leuchtraketen.cloud 'kubectl rollout restart deploy -n lr_messagehub'
